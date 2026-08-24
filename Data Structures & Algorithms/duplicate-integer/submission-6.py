91221c43f86537c48d66c197190c0a14d39f8a85
Divit Chopra <156983333+DivitC25@users.noreply.github.com>
2026-08-24 16:07:29 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums):
+        seen = set()
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+            else:
+                return True
+        return False
