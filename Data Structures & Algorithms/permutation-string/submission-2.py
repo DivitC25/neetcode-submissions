d8415192c026c44a2cb24174a3164bfea9f586e7
Divit Chopra <156983333+DivitC25@users.noreply.github.com>
2026-08-30 14:46:41 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        
+        windowSize = len(s1)
+        setOne = set(s1)
+        for i in range(len(s2) - windowSize + 1):
+            subWord = s2[i : i + windowSize]
+            if set(subWord) == set(s1) and self.wordFrequency(subWord) == self.wordFrequency(s1):
+                return True
+        
+        return False
+    
+    def wordFrequency(self, word):
+        wordDict = {}
+        word = ''.join(sorted(word))
+        for letter in word:
+            if letter in wordDict:
+                wordDict[letter] += 1
+            else:
+                wordDict[letter] = 1
+        
+        return frozenset(tuple(wordDict.items()))
+
+            
